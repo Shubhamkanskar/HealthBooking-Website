8f76024ea729c08d34d416057423085133f388bd
shubham kanaskar <shubhamkanaskar75@gmail.com>
2024-06-04 08:09:11 +0530

    added some changes

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,10 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
